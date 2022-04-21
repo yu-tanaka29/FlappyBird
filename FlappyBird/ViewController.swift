b9f531089198437c0ed619110cfa2e44d3b1f687
yuki.tanaka <yu-tanaka@i-enter.co.jp>
2022-04-21 11:24:27 +0900

    Initial Commit

---
+++ FlappyBird/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlappyBird
+//
+//  Created by 田中 勇輝 on 2022/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
